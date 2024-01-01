1ab3a3f0540344b8074f23c514f482529af33b16
Sourov Sharma <sourovsharma31@gmail.com>
2024-01-01 16:37:43 +0600

    Update lcd.c

--- lcd.c
+++ lcd.c
@@ -1,7 +1,9 @@
 /*
+----------------------------------------------------------------
 Author  : Sourov Sharma
 Date    : 2023 November
 Details : A library for atmega328p for LCD display.
+----------------------------------------------------------------
 */
 
 #include "lcd.h"
